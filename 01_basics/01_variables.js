e50933dc9e82c62eaa95ed619fd52db95a501146
harshitsharma2405 <sharma.harshit1620@gmail.com>
2025-05-19 13:52:47 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553           // cannot be changed 
+let accountEmail = "har.sh@gmail.com"
+var accountPassword = "12345"          // do not use var
+accountCity = "Bareilly"               // prefer not to assign like this
+let accountState; 
+
+// accountId = 2 
+console.log(accountId);
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+accountEmail= "sharma.harshit1620@gmail.com"
+accountPassword = 1242331
+accountCity ="Noida"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
