024eda50a60d4d93a368039275d4843dcda25ee3
harshitsharma2405 <sharma.harshit1620@gmail.com>
2025-05-19 15:30:49 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";   // treat all JS code as newer version
+
+// alert(3+3)    // we are using node.js, not browser 
+
+// number => 2 to power 53
+// BigInt
+//  string =>""
+// boolean => true/false
+// undefined => 
+// Symbol 
+
+
+// object 
+
+console.log(typeof)
